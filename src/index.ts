e9791d7e73da8f8c88316ad976f80d8a6cc5abd8
Roi Avidar <roiavidar@gmail.com>
2021-01-22 21:33:42 +0200

    extract star css to common

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import MovingBallContainer from './MovingBalls/MovingBallsContainer';
+import "./styles/common.scss";
 
 const root = document.getElementById('root');
 const mb = new MovingBallContainer(root);
